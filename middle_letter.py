a8e9ccd7edd79b67e45af6ca7e817ee0d539f703
elnaz <elnaz.nasiri92@gmail.com>
2022-12-02 00:50:34 +0330

    Add New Solution

--- middle_letter.py
+++ middle_letter.py
@@ -1,7 +1,19 @@
+# solution 1
 def mid(str):
     if len(str) % 2 == 1:
         i = len(str)//2
         return str[i]
     return ""
-print(mid("salam"))
+print(mid("abcd"))
+
+
+# solution 2
+def mid(str):
+    if len(str) % 2 == 0:
+        return ""
+    else:
+        return str[len(str)//2]
+
+print(mid("abc"))
+
 
