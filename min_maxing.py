331dc20f36a7ebbc26390a182be2c43d67e3318e
elnaz <elnaz.nasiri92@gmail.com>
2022-12-10 21:40:59 +0330

    Add New Solution

--- min_maxing.py
+++ min_maxing.py
@@ -4,4 +4,15 @@ def largest_difference(list1):
         maxNumber=max(list1)
         minNumber=min(list1)
     return maxNumber-minNumber
+    
 print(largest_difference([1, 2, 3]))
+
+
+# solution 2
+def largest_difference(list1):
+    list1.sort()
+    minList = list1[0]
+    maxList = list1[-1]
+    return maxList - minList
+
+print(largest_difference([5,8,9,12,1]))
