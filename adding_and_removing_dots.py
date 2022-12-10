29f8d7633f6fd588b0ef73fb9ec55d1db105b4d3
elnaz <elnaz.nasiri92@gmail.com>
2022-12-10 21:37:47 +0330

    Add New Solution

--- adding_and_removing_dots.py
+++ adding_and_removing_dots.py
@@ -1,3 +1,4 @@
+# solution 1
 def add_dots(str):
     jointString = ".".join(str)
     return jointString
@@ -6,5 +7,18 @@ def remove_dots(str):
     splitString = str.split(".")
     joinString = "".join(splitString)
     return joinString
+
+print(remove_dots(add_dots("test")))
+
+
+# solution 2
+def add_dots(str):
+    temp = ""
+    for letter in str:
+        temp += letter + "."
+    return temp[:-1]
+
+def remove_dots(str):
+    return str.replace(".", "")
 
 print(remove_dots(add_dots("test")))
