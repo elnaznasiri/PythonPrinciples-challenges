fcb94805a7d36c658ed0f73ee33e62c1ff2de9b9
elnaz <elnaz.nasiri92@gmail.com>
2022-12-17 06:12:28 +0330

    Add New Solution

--- custom_zip.py
+++ custom_zip.py
@@ -1,7 +1,22 @@
+# solution 1
 def zap(a, b):
     result = []
     for i in range(len(a)) :
         tuple_result = (a[i],b[i])
+        result.append(tuple_result)
+    return result
+
+print(zap(
+    [0, 1, 2, 3],
+    [5, 6, 7, 8]
+))
+
+
+# solution 2
+def zap(a, b):
+    result = []
+    for i,j in zip(a, b):
+        tuple_result = (i,j)
         result.append(tuple_result)
     return result
 
